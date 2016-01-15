bbf2e8ae3f43daf9247daed28e5b2cf305af6545
imanavaz <i_avazpour@yahoo.com>
2016-01-15 13:16:09 +1100

    working on column cloning. Slope is not working yet.

--- Beaver/Scripts/controlpanel-logic.js
+++ Beaver/Scripts/controlpanel-logic.js
@@ -3,7 +3,7 @@ var selectedShapeProperty = "";
 var currShape;//to keep the shpae being designed
 
 
-function myFunction()
+function myFunction()//apply a property on a specific shape
 {
     updateShapeByCell(currShape, 0, 2);
 }
@@ -47,9 +47,118 @@ function updateShapeByCell(shape, i, j) {
 }
 
 
-function applyOnGroup()
+function myFunction2() //clone a shape using a shape and for a data range 
 {
+    //applyOnGroup(currShape, 1, 1, 2, 2);
+    cloneByColumn(currShape, selectedShapeType, selectedShapeProperty, 2);
+}
 
+function cloneByColumn(shape1, shapeType, prop, column) {
+
+    var data = dataGrid.getDataAtCol(column);
+
+    var tempshape = shape1;
+    var tempshapeBBox = tempshape.getBBox();
+
+    paper.clear();//clear canvas
+
+    var i,
+        len = data.length;
+    for (i = 0; i < len; i++)
+    {
+        var shape;
+
+        if (data[i] != null)
+        {
+            if ((prop == "height") && (shapeType == "rect")) {
+
+                shape = paper.rect().attr({
+                    x : tempshapeBBox.x + (i * (tempshapeBBox.width+2)), 
+                    y : tempshapeBBox.y, 
+                    width : tempshapeBBox.width, 
+                    height : data[i],
+                    fill: "orange"
+                });
+
+                shape.click(function () {
+                    currShape = shape;
+                    selectedShapeType = "rect";
+                    document.getElementById("rect-radio").checked = true;
+                });
+
+                //console.log(shape);
+
+                tempshape = shape;
+            }
+            else if ((prop == "width") && (shapeType == "rect")) {
+
+                shape = paper.rect().attr({
+                    x: tempshapeBBox.x, 
+                    y: tempshapeBBox.y + (i * (tempshapeBBox.height + 2)),
+                    width: data[i],
+                    height: tempshapeBBox.height,
+                    fill: "orange"
+                });
+
+                shape.click(function () {
+                    currShape = shape;
+                    selectedShapeType = "rect";
+                    document.getElementById("rect-radio").checked = true;
+                });
+
+                tempshape = shape;
+            }
+            else if ((prop == "radius") && (shapeType == "circle"))
+            {
+                shape = paper.circle().attr({
+                    cx: tempshapeBBox.x2 + parseInt(data[i]),
+                    cy: tempshapeBBox.y + tempshapeBBox.height / 2,
+                    r: data[i],
+                    fill: "blue"
+                });
+                
+                shape.click(function () {
+                    currShape = shape;
+                    selectedShapeType = "circle";
+                    document.getElementById("circle-radio").checked = true;
+                });
+
+                console.log();
+
+                tempshape = shape;
+                tempshapeBBox = tempshape.getBBox();//update bounding box
+            }
+            else if ((shapeType == "line") && (prop == "slope"))
+            {
+                var teta = data[i];
+
+                var l = Math.floor(shape.getTotalLength() / 2);
+                var startpoint = shape.getPointAtLength(1);
+
+                b = l * Math.cos(teta);
+                var x2 = b + startpoint.x;
+
+                a = l * Math.sin(teta);
+                var y2 = a + startpoint.y;
+
+                var pathString = 'M' + startpoint.x + ',' + startpoint.y + 'L' + x2 + ',' + y2 + 'Z';
+                shape.attr("path", pathString);
+
+                //console.log(pathString);
+            }
+        }
+    }
+
+    //console.log(data);
+}
+
+
+
+function applyOnGroup(shape, fromRow, fromColumn, toRow, toColomn)
+{
+    var data = dataGrid.getData(fromRow, fromColumn, toRow, toColomn);
+
+    //console.log(data);
 }
 
 
