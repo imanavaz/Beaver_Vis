aed3b299cde2e28d6e3699cae60ece70d9b4deb8
imanavaz <i_avazpour@yahoo.com>
2016-02-03 11:14:26 +1100

    Working on inference engine preparations -> adding manipulations and interactions to shapes. Not a working copy.

--- Beaver/Scripts/canvaslogic.js
+++ Beaver/Scripts/canvaslogic.js
@@ -6,6 +6,8 @@ var eventStartPointY;
 var eventEndPointX;
 var eventEndPointY;
 
+var ox = null;
+var oy = null;
 
 /*Mouse Events*/
 
@@ -16,6 +18,46 @@ paper.canvas.onmousedown = function(event) {
     //console.log('Down: ' + eventStartPointX, eventStartPointY);
 };
 
+
+var hoverIn = function () {
+    this.attr({ "fill": "#E3E3E3" });
+};
+
+
+var hoverOut = function () {
+    this.attr({ "fill": "#000" });
+}
+
+
+function drag_start(e) {
+
+};
+
+function drag_move(dx, dy, posx, posy) {
+
+    //shape.attr({
+    //    fill: "#fa0"
+    //});
+    //
+    // Here's the interesting part, apply an absolute transform 
+    // with the dx,dy coordinates minus the previous value for dx and dy
+    //
+    this.attr({
+        transform: "...T" + (dx - ox) + "," + (dy - oy)
+    });
+    //
+    // store the previous versions of dx,dy for use in the next move call.
+    //
+    ox = dx;
+    oy = dy;
+}
+
+
+function drag_up(e) {
+    // don't forget to reset the original positions.
+    ox = 0;
+    oy = 0;
+}
 
 paper.canvas.onmouseup = function (event) {
 
@@ -28,13 +70,20 @@ paper.canvas.onmouseup = function (event) {
     var h = Math.abs(eventEndPointY - eventStartPointY);
 
     if (selectedShapeType == "rect") {
-        var rect = paper.rect(eventStartPointX, eventStartPointY, w, h).attr({ fill: "orange" });
+        var rect = paper.rect(eventStartPointX, eventStartPointY, w, h);//.attr({ fill: "orange" });
 
         rect.click(function () {
             currShape = rect;
             selectedShapeType = "rect";
             document.getElementById("rect-radio").checked = true;
         });
+
+        rect.hover(hoverIn, hoverOut, rect, rect);
+        //rect.drag(drag_move, drag_start, drag_up);
+        var ft = paper.freeTransform(rect);
+        // Show hidden freeTransform handles
+        ft.showHandles();
+
 
         currShape = rect;
     }
